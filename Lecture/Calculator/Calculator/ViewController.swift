f8c2ec877df69b1a86551843f6c4e7c49c0ef834
valdarin <valdarin@gmail.com>
2016-05-08 21:47:41 -0700

    initial project creation

---
+++ Lecture/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Scott Moen on 5/8/16.
+//  Copyright © 2016 Scott Moen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
