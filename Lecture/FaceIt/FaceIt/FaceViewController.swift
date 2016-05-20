32b18ebf0e17171101d19c033112eadc03bf868e
valdarin <valdarin@gmail.com>
2016-05-19 22:25:33 -0700

    new project

---
+++ Lecture/FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Scott Moen on 5/19/16.
+//  Copyright © 2016 Scott Moen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
