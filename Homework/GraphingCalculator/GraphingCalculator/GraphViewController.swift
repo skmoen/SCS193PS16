e8f5407c3ffca1fccfc16bfbd53639c0dd1e8d6a
valdarin <valdarin@gmail.com>
2016-06-13 23:57:24 -0700

    add 3 gestures to graph for pan, tap, and pinch

--- Homework/GraphingCalculator/GraphingCalculator/GraphViewController.swift
+++ Homework/GraphingCalculator/GraphingCalculator/GraphViewController.swift
@@ -8,6 +8,19 @@
 
 import UIKit
 
+
+// MARK: - CGPoint arithmetic
+
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func +=(inout left: CGPoint, right: CGPoint) {
+    left = left + right
+}
+
+// MARK: - GraphViewController
+
 class GraphViewController: UIViewController, GraphViewDataSource {
 
     var program: AnyObject? {
@@ -26,6 +39,33 @@ class GraphViewController: UIViewController, GraphViewDataSource {
         }
     }
     
+    // MARK: - UIGestureRecognizers
+
+    @IBAction func pan(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .Changed, .Ended:
+            graphView.origin += sender.translationInView(graphView)
+            sender.setTranslation(CGPointZero, inView: graphView)
+        default:
+            break
+        }
+    }
+    
+    @IBAction func pinch(sender: UIPinchGestureRecognizer) {
+        switch sender.state {
+        case .Changed, .Ended:
+            graphView.scale *= sender.scale
+            sender.scale = 1
+        default: break
+        }
+    }
+    
+    @IBAction func tap(sender: UITapGestureRecognizer) {
+        if sender.state == .Ended {
+            graphView.origin = sender.locationInView(graphView)
+        }
+    }
+
     // MARK: - GraphViewDataSource
     func calculateValue(x x: Double) -> Double {
         brain.varialbeValues["M"] = x
